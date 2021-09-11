f1da4306fcb26de67fbe3d8a795f4d9f08636185
EvgeniyaBarabash <barabash1989@gmail.com>
2021-09-11 18:40:41 +0300

    вынесла ref

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
+import refs from './js/refs.js';
+const { daysRef, hoursRef, minsRef, secsRef }=refs;
 class CountdownTimer {
   constructor(options) {
-    this.selector = options.selector;
     this.targetDate = options.targetDate;
-  }
-  changeTime() {
-    const refs = document.querySelector(`${this.selector}`);
-
-    const daysRef = refs.querySelector('[data-value = "days"]');
-    const hoursRef = refs.querySelector('[data-value = "hours"]');
-    const minsRef = refs.querySelector('[data-value = "mins"]');
-    const secsRef = refs.querySelector('[data-value = "secs"]');
-
-    return { daysRef, hoursRef, minsRef, secsRef };
   }
   updateClockFace(time) {
     this.updateTime(time);
@@ -30,11 +21,10 @@ class CountdownTimer {
     const mins = pad(Math.floor((deltaTime % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = pad(Math.floor((deltaTime % (1000 * 60)) / 1000));
 
-    const refs = this.changeTime();
-    refs.daysRef.textContent = days;
-    refs.hoursRef.textContent = hours;
-    refs.minsRef.textContent = mins;
-    refs.secsRef.textContent = secs;
+    daysRef.textContent = days;
+    hoursRef.textContent = hours;
+    minsRef.textContent = mins;
+    secsRef.textContent = secs;
     function pad(value) {
       return String(value).padStart(2, "0");
     }
@@ -47,11 +37,10 @@ class CountdownTimer {
     this.clearClockFace();
   }
   clearClockFace(time) {
-    const refs = this.changeTime();
-    refs.daysRef.textContent = "00";
-    refs.hoursRef.textContent = "00";
-    refs.minsRef.textContent = "00";
-    refs.secsRef.textContent = "00";
+   daysRef.textContent = "00";
+   hoursRef.textContent = "00";
+   minsRef.textContent = "00";
+   secsRef.textContent = "00";
   }
 }
 const timer = new CountdownTimer({
